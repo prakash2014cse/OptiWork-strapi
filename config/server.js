e28301b2fa29b511042215c1ae84b8697e791ad3
prakash2014cse@gmail.com <prakash2014cse@gmail.com>
2021-03-07 19:58:01 +0530

    URL changes

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  //  url: "http://localhost:1337/",
+   url: "http://161.35.145.62:1337/",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd6b726dfab3b9ffbeb465d37567d481f'),
